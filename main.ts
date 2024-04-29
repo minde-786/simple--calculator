68712f42f9ad44aa800313a9c0619586ffbb947b
NASRULLAH <you@example.com>
2024-04-29 23:38:17 +0500

    push my calculator in github

---
+++ main.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const input=await inquirer.prompt([
+   {name:"num1",
+    type:"number",
+    message:"Enter frist number",
+   },
+   { name:"num2",
+   type:"number",
+   message:"Enter sceond number",
+
+   },
+   { name:"operator",
+   type:"list",
+   choices:["addition","multiplication","division","substraction"],
+message:"select one of the given option",
+
+   }
+])
+if (input.operator=== "addition"){
+    console.log(input.num1+input.num2);
+
+}
+else if(input.operator==="substraction"){
+    console.log(input.num1-input.num2);
+}
+else if(input.operator==="division"){
+ console.log(input.num1/input.num2);   
+}
+else if(input.operator==="multiplication"){
+    console.log(input.num1*input.num2);
+}
